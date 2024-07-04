beceba2594c1af8ed50bb0ba58e6487ef70a5f34
lowkeyOwO <lokesh.varathan1593@gmail.com>
2024-07-04 13:39:03 +0530

    static

--- app/page.tsx
+++ app/page.tsx
@@ -10,118 +10,137 @@ interface QuoteObj {
 
 const quotesData: { [key: string]: { quote: string; director: string } } = {
   "1": {
-      "quote": "You keep on learning. It doesn’t stop. I think the minute I stop feeling that I’m going to, you know — stop learning — I would stop being a filmmaker.",
-      "director": "Deepa Mehta"
+    quote:
+      "You keep on learning. It doesn’t stop. I think the minute I stop feeling that I’m going to, you know — stop learning — I would stop being a filmmaker.",
+    director: "Deepa Mehta",
   },
   "2": {
-      "quote": "I’m not a master; I’m just a hard-working filmmaker. I would like everyone to see me as a friend rather than a master.",
-      "director": "John Woo"
+    quote:
+      "I’m not a master; I’m just a hard-working filmmaker. I would like everyone to see me as a friend rather than a master.",
+    director: "John Woo",
   },
   "3": {
-      "quote": "Create your own visual style. Let it be unique for yourself and yet identifiable for others.",
-      "director": "Orson Welles"
+    quote:
+      "Create your own visual style. Let it be unique for yourself and yet identifiable for others.",
+    director: "Orson Welles",
   },
   "4": {
-      "quote": "When you love something it’s not a job anymore.",
-      "director": "Spike Lee"
+    quote: "When you love something it’s not a job anymore.",
+    director: "Spike Lee",
   },
   "5": {
-      "quote": "Without risks you don’t go anywhere, you don’t learn anything, and the movies that have been least enjoyable for me have been the ones that have kind of been by rote. Directors should always explore their boundaries — that’s where really exciting things happen.",
-      "director": "Ellen Kuras"
+    quote:
+      "Without risks you don’t go anywhere, you don’t learn anything, and the movies that have been least enjoyable for me have been the ones that have kind of been by rote. Directors should always explore their boundaries — that’s where really exciting things happen.",
+    director: "Ellen Kuras",
   },
   "6": {
-      "quote": "It’s cool for me because I’m a director, but I’m also a teacher. I’m a lover of cinema, and I love working with people who are hungry and have the energy to really do better work.",
-      "director": "John Singleton"
+    quote:
+      "It’s cool for me because I’m a director, but I’m also a teacher. I’m a lover of cinema, and I love working with people who are hungry and have the energy to really do better work.",
+    director: "John Singleton",
   },
   "7": {
-      "quote": "A good director creates an environment, which gives the actor the encouragement to fly.",
-      "director": "Kevin Bacon"
+    quote:
+      "A good director creates an environment, which gives the actor the encouragement to fly.",
+    director: "Kevin Bacon",
   },
   "8": {
-      "quote": "I love more than anything looking at a movie scene by scene and seeing the intention behind it. It allows you to really appreciate the hand of the filmmaker.",
-      "director": "Jodie Foster"
+    quote:
+      "I love more than anything looking at a movie scene by scene and seeing the intention behind it. It allows you to really appreciate the hand of the filmmaker.",
+    director: "Jodie Foster",
   },
   "9": {
-      "quote": "A director makes 100 decisions an hour. Students ask me how you know how to make the right decision, and I say to them, ‘If you don’t know how to make the right decision, you’re not a director.'",
-      "director": "George Lucas"
+    quote:
+      "A director makes 100 decisions an hour. Students ask me how you know how to make the right decision, and I say to them, ‘If you don’t know how to make the right decision, you’re not a director.'",
+    director: "George Lucas",
   },
   "10": {
-      "quote": "Be passionate and move forward with gusto every single hour of every single day until you reach your goal.",
-      "director": "Ava DuVernay"
+    quote:
+      "Be passionate and move forward with gusto every single hour of every single day until you reach your goal.",
+    director: "Ava DuVernay",
   },
   "11": {
-      "quote": "Never give up. If you want to become one, you have to be really, really strong, never give up, because you’re going to have so many ‘nos.’ When I wrote my first screenplay I was 17, but when I directed my first film I was 36. It gives you an idea how long it takes.",
-      "director": "Julie Delpy"
+    quote:
+      "Never give up. If you want to become one, you have to be really, really strong, never give up, because you’re going to have so many ‘nos.’ When I wrote my first screenplay I was 17, but when I directed my first film I was 36. It gives you an idea how long it takes.",
+    director: "Julie Delpy",
   },
   "12": {
-      "quote": "Pick up a camera. Shoot something. No matter how small, no matter how cheesy, no matter whether your friends and your sister star in it. Put your name on it as director. Now you’re a director. Everything after that you’re just negotiating your budget and your fee.",
-      "director": "James Cameron"
+    quote:
+      "Pick up a camera. Shoot something. No matter how small, no matter how cheesy, no matter whether your friends and your sister star in it. Put your name on it as director. Now you’re a director. Everything after that you’re just negotiating your budget and your fee.",
+    director: "James Cameron",
   },
   "13": {
-      "quote": "To be an artist means never to avert one’s eyes.",
-      "director": "Akira Kurosawa"
+    quote: "To be an artist means never to avert one’s eyes.",
+    director: "Akira Kurosawa",
   },
   "14": {
-      "quote": "If it can be written or thought, it can be filmed.",
-      "director": "Stanley Kubrick"
+    quote: "If it can be written or thought, it can be filmed.",
+    director: "Stanley Kubrick",
   },
   "15": {
-      "quote": "Don’t give me any money, don’t give me any people, but give freedom, and I’ll give you a movie that looks gigantic.",
-      "director": "Robert Rodriguez"
+    quote:
+      "Don’t give me any money, don’t give me any people, but give freedom, and I’ll give you a movie that looks gigantic.",
+    director: "Robert Rodriguez",
   },
   "16": {
-      "quote": "I try to push ideas away, and the ones that will not leave me alone are the ones that ultimately end up happening.",
-      "director": "J.J. Abrams"
+    quote:
+      "I try to push ideas away, and the ones that will not leave me alone are the ones that ultimately end up happening.",
+    director: "J.J. Abrams",
   },
   "17": {
-      "quote": "For me, shooting, editing, and scoring rely on rhythm.",
-      "director": "Donnie Yen"
+    quote: "For me, shooting, editing, and scoring rely on rhythm.",
+    director: "Donnie Yen",
   },
   "18": {
-      "quote": "As a filmmaker, I always try not to concern myself with the outcome of things. I make the movie, and I do that as honestly and good as I can. I don’t want to pollute my thoughts with what is going to happen with it afterwards, because I have to work inside-out.",
-      "director": "Susanne Bieris"
+    quote:
+      "As a filmmaker, I always try not to concern myself with the outcome of things. I make the movie, and I do that as honestly and good as I can. I don’t want to pollute my thoughts with what is going to happen with it afterwards, because I have to work inside-out.",
+    director: "Susanne Bieris",
   },
   "19": {
-      "quote": "One of the things you do as a writer and as a filmmaker is grasp for resonant symbols and imagery without necessarily fully understanding it yourself.",
-      "director": "Christopher Nolan"
+    quote:
+      "One of the things you do as a writer and as a filmmaker is grasp for resonant symbols and imagery without necessarily fully understanding it yourself.",
+    director: "Christopher Nolan",
   },
   "20": {
-      "quote": "I always want to make films. I think of it as a great opportunity to comment on the world in which we live.",
-      "director": "Kathryn Bigelow"
+    quote:
+      "I always want to make films. I think of it as a great opportunity to comment on the world in which we live.",
+    director: "Kathryn Bigelow",
   },
   "21": {
-      "quote": "Humans have both the urge to create and destroy.",
-      "director": "Hayao Miyazaki"
+    quote: "Humans have both the urge to create and destroy.",
+    director: "Hayao Miyazaki",
   },
   "22": {
-      "quote": "There is no terror in the bang, only in the anticipation of it.",
-      "director": "Alfred Hitchcock"
+    quote: "There is no terror in the bang, only in the anticipation of it.",
+    director: "Alfred Hitchcock",
   },
   "23": {
-      "quote": "This whole world is wild at heart and weird on top.",
-      "director": "David Lynch"
+    quote: "This whole world is wild at heart and weird on top.",
+    director: "David Lynch",
   },
   "24": {
-      "quote": "The journey of filmmaking is so amazing. You start off with great confidence, and develop insecurity at the time of release. When you are ready with the finished product, you are constantly wondering if you have been honest to the story you started out with, if you got what you wanted. One is too close to the project by then to be objective.",
-      "director": "Mani Ratnam"
+    quote:
+      "The journey of filmmaking is so amazing. You start off with great confidence, and develop insecurity at the time of release. When you are ready with the finished product, you are constantly wondering if you have been honest to the story you started out with, if you got what you wanted. One is too close to the project by then to be objective.",
+    director: "Mani Ratnam",
   },
   "25": {
-      "quote": "Cinema is an art form that is designed to go across borders. And as a filmmaker, the only way I can direct a movie is when I feel close to my culture.",
-      "director": "Denis Villeneuve"
+    quote:
+      "Cinema is an art form that is designed to go across borders. And as a filmmaker, the only way I can direct a movie is when I feel close to my culture.",
+    director: "Denis Villeneuve",
   },
   "26": {
-      "quote": "My favourite virtue is not having any.",
-      "director": "Ram Gopal Varma"
+    quote: "My favourite virtue is not having any.",
+    director: "Ram Gopal Varma",
   },
   "27": {
-      "quote": "Cinema is a mirror by which we often see ourselves.",
-      "director": "Alejandro Gonzalez Inarritu"
+    quote: "Cinema is a mirror by which we often see ourselves.",
+    director: "Alejandro Gonzalez Inarritu",
   },
   "28": {
-      "quote": "I don't find anything black and white; I find grey in every person, and that is what excites me.",
-      "director": "Vetrimaaran"
-  }
-}
+    quote:
+      "I don't find anything black and white; I find grey in every person, and that is what excites me.",
+    director: "Vetrimaaran",
+  },
+};
 
 const getQuote = (): QuoteObj => {
   const randomIndex = Math.floor(Math.random() * 28) + 1;
@@ -132,11 +151,11 @@ const getQuote = (): QuoteObj => {
 
 export default function Login() {
   const { quote, director } = getQuote();
-  console.log( "login Image:",loginimg)
   return (
     <div className="w-screen h-screen grid grid-rows-1 md:grid-cols-5 ">
       <div className="w-full h-full bg-white md:h-screen md:w-full md:col-span-3">
         <Image
+          unoptimized
           src={loginimg}
           alt="cinema"
           className="h-screen md:h-full"
@@ -161,4 +180,3 @@ export default function Login() {
     </div>
   );
 }
-
--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ["https://image.tmdb.org/","http://127.0.0.1","https://127.0.0.1"],
+    domains: ["https://image.tmdb.org/"],
   },
 };
 
